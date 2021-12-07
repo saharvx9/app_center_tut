1e0290ff15ed4d817d0e04eb22383ba9e15f7c5b
Sahar Vanunu <saharv@nayax.com>
2021-12-07 15:47:45 +0200

    add app center + post clone

---
+++ android/app/appcenter-post-clone.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# place this script in project/android/app/
+cd ..
+# fail if any command fails
+set -e
+# debug log
+set -x
+
+cd ..
+# choose a different release channel if you want - https://github.com/flutter/flutter/wiki/Flutter-build-release-channels
+# stable - recommended for production
+
+git clone -b stable https://github.com/flutter/flutter.git
+export PATH=`pwd`/flutter/bin:$PATH
+
+flutter channel stable
+flutter doctor
+
+flutter build apk --release
+
+# if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
+flutter build appbundle --release
+
+echo "move to custom path apk"
+#build/app/outputs/flutter-apk
+mkdir -p `pwd`/build/outputs/apk/; mv build/app/outputs/flutter-apk/app-release.apk $_
+
+echo "move to custom path bundle"
+mkdir -p `pwd`/build/outputs/bundle/; mv build/app/outputs/bundle/release/app-release.aab $_
---
+++ android/app/src/main/kotlin/com/example/app_center_tut/App.java
@@ -0,0 +1,18 @@
+package com.example.app_center_tut;
+
+import static com.microsoft.appcenter.AppCenter.*;
+
+import android.app.Application;
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        start(this, "511f74e5-a948-47ee-b239-6b68437e1757",
+                Analytics.class, Crashes.class);
+    }
+}
